ae23d2ec0983a449d3eea572f8360d70c0582e86
Santiago Forero <santiago@forero.ga>
2021-09-02 13:11:00 -0500

    inicial

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/theme.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet"> 
+    <script src="https://kit.fontawesome.com/89178b42f5.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <nav id="main-nav">
+        <a href="/">
+            <i class="fa fa-home"></i> Home
+        </a>
+        <a href="http://192.3.31.108:31598/">
+            <i class="fas fa-map-marker"></i> Mapa
+        </a>
+    </nav>
+    
+    <header id="main-header">
+            <h1>UNCRAFT.CRAFTED.PRO</h1>
+    </header>
+
+    <div id="content">
+        <h2>Lorem ipsum </h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, incidunt cum sequi ipsum aspernatur deserunt voluptate atque odit accusamus dignissimos voluptas rerum aperiam voluptatibus, hic ipsam sapiente doloribus, reiciendis ut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis explicabo asperiores magni ea cumque autem consequuntur, quis dolorem tenetur ab culpa optio voluptate voluptatibus dolorum beatae maiores adipisci nemo.</p>
+        <a href="sdfasd">link</a>
+
+        <h3>Lorem ipsum</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, incidunt cum sequi ipsum aspernatur deserunt voluptate atque odit accusamus dignissimos voluptas rerum aperiam voluptatibus, hic ipsam sapiente doloribus, reiciendis ut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis explicabo asperiores magni ea cumque autem consequuntur, quis dolorem tenetur ab culpa optio voluptate voluptatibus dolorum beatae maiores adipisci nemo.</p>
+    </div>
+
+    <footer id="main-footer">
+        <p>UN Minecraft</p>
+            <a href="https://www.instagram.com/minecraft_un/"><i class="fa fa-instagram"></i></a>
+   
+            <a href="https://discord.gg/9NtGSAyDV9"><i class="fab fa-discord"></i></a>     
+    </footer>
+</body>
+</html>
