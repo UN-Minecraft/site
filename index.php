6297620f7ad841f18944c788083f6fd8f39751a7
Santiago Forero <santiago@forero.ga>
2021-09-10 16:57:38 -0500

    update index.php

--- index.php
+++ index.php
@@ -23,9 +23,32 @@
         <p>Este es el servidor de Minecraft de la Universidad Nacional de Colombia, hecho por estudiantes para estudiantes.</p>
         <p>Nuestro principal objetivo es construir las sedes de la UNAL en Minecraft, hasta el momento gracias a nuestros preciados builders se ha construido la sede Bogotá y Medellín casi en su totalidad. Estamos abiertos a recibir nuevos builders que quieran corregir o hacer alguna parte de nuestro lindo campus.</p>
 
-        <p>Por primera vez, el servidor utilizará mods, esta es una etapa experimental en la que probaremos el rendimiento y alcance del servidor, para más información ver <a href="mods.php">Mods</a>.</p>
+        <p>
+            Bienvenidos a esta nueva entrega del servidor, un periodo de Mods experimental, de rol y de nuevas dinámicas para jugar con nuestros amigos y conocernos un poco mejor entre todos. La temática en esta ocasión es UN, por tanto, la plaza central es nuestra icónica plaza Che, donde encontrarás distintos aldeanos donde podrás intercambiar esmeraldas por comida encantamientos y en algunos días por armaduras y demás.
+        </p>
 
-        <p></p>
+        <img src="img/1.png" alt="">
+
+        <p>
+            En esta ocasión las piedras de protección no existirán, por lo cual el concepto de construcción por parcelas saldrá a relucir, en esta modalidad, cada persona se le asignará una parcela para construir su casa de los sueños a mano de los nuevos mods de decoración, cada dueño de parcela puede construir tanto para arriba como para abajo, siempre respetando el espacio de las parcelas contiguas, por tanto, construcciones que no respeten dicha división serán recortadas sin previo aviso. <b>PD:</b> El cartel de propiedad se encontrará en la esquina inferior izquierda de cada parcela.
+        </p>
+
+        <p>
+            Como no existen piedras de protección, la alternativa para la seguridad es por medio del mod Security Craft, con este mod podrás crear puertas con contraseña, con retina, etc, además de cofres protegidos, por tanto RECOMENDAMOS ENCARECIDAMENTE, proteger tus cofres lo antes posible, de igual forma, el robo no está permitido, ni la destrucción de bloques de parcelas diferentes a la propia, cualquier persona que sea denunciada o sea encontrada infringiendo estas normas, será castigado severamente.
+        </p>
+
+        <img src="img/3.png" alt="">
+
+        <p>
+            Como la extensión de la parcela constituye en todo el eje Z, la recolección de materiales tendrá lugar por fuera de estas, por tanto, la construcción de minas en las propias parcelas, solo pueden ser del largo de esta misma. Existirá un warp a una mina publica alejada de las parcelas para que puedan minar sin temor alguno, de igual forma existirán distintos puentes que permitirán la entrada y salida de las parcelas para la exploración del mundo exterior.
+        </p>
+
+        <img src="img/4.png" alt="">
+
+        <p>
+            Como estamos hablando de un contexto residencial, la construcción de granjas en las parcelas serán reducidas, de igual forma la cantidad de animales por especie, serán limitadas, finalmente la redstone permitida también será reducida, por tanto se recomienda evitar automatizar en lo máximo posible, debido a que se pueden presentar picos de lag y tirones de fps, al observar este tipo de situaciones por parte de administración, se pueden retirar los elementos sin previo aviso.
+        </p>
+
 
         <h3>Links</h3>
         <ul>
